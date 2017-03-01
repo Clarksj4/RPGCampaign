eed07a4822e74377b8cedb0c0db9ee2b467a1846
Clarksj4 <clarksj4@student.op.ac.nz>
2017-03-01 17:54:05 +1300

    Simplify range class
    remove drawer

--- Assets/Scripts/Range.cs
+++ Assets/Scripts/Range.cs
@@ -1,62 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using UnityEngine;
 
 [Serializable]
 public class Range
 {
-    public float Min
-    {
-        get { return min; }
-        set
-        {
-            min = value;
-            if (min > Max) throw new ArgumentException("Minimum range bound cannot exceed maximum");
-            if (current < min) current = min;
-        }
-    }
-
-    public float Current
-    {
-        get { return current; }
-        set
-        {
-            current = value;
-            if (!Contains(current)) throw new ArgumentException("Current value is outside of range");
-        }
-    }
-
-    public float Max
-    {
-        get { return max; }
-        set
-        {
-            max = value;
-            if (min > Max) throw new ArgumentException("Maximum range bound cannot be less than minimum");
-            if (current > max) current = max;
-        }
-    }
-
-    [SerializeField]
-    [HideInInspector]
-    private float min;
-    [SerializeField]
-    [HideInInspector]
-    private float current;
-    [SerializeField]
-    [HideInInspector]
-    private float max;
+    public float Min;
+    public float Current;
+    public float Max;
 
     public bool Contains(float x)
     {
-        return Contains(min, max, x);
+        return Contains(Min, Max, x);
     }
 
     public float Size()
     {
-        return Size(min, max);
+        return Size(Min, Max);
     }
 
     public static bool Contains(float min, float max, float x)
