381b4284a2553bbb2a63cfa221b57f80a2b57192
Stephen <clarksj4@student.op.ac.nz>
2017-03-02 18:46:03 +1300

    Begin pathfinding code

---
+++ Assets/MyStuff/Scripts/HexMapPathfinding/HexCellExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public static class HexCellExtension
+{
+    public static Vector3[] GetCorners(this HexCell cell)
+    {
+        Vector3[] corners = new Vector3[6];
+        for (int i = 0; i < corners.Length; i++)
+            corners[i] = cell.Position + HexMetrics.corners[i];
+
+        return corners;
+    }
+}
---
+++ Assets/MyStuff/Scripts/HexMapPathfinding/Pathfinder.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+    public HexGrid hexGrid;
+
+	public List<HexCell> FindPath(Vector3 origin, Vector3 destination)
+    {
+        return null;
+    }
+}
