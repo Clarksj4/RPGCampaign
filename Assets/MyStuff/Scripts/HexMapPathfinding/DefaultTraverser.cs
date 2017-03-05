6a1600b718d565f13e65f5f874e2a7397d7d19c3
Clarksj4 <clarksj4@student.op.ac.nz>
2017-03-05 23:08:19 +1300

    Add additional traversal costs

--- Assets/MyStuff/Scripts/HexMapPathfinding/DefaultTraverser.cs
+++ Assets/MyStuff/Scripts/HexMapPathfinding/DefaultTraverser.cs
@@ -18,9 +18,49 @@ public class DefaultTraverser : ITraverser
 
     public float TraverseCost(HexCell cell, HexDirection direction)
     {
-        if (cell.HasRoadThroughEdge(direction))
-            return 0f;
+        HexCell neighbour = cell.GetNeighbor(direction);
 
-        return 1f;
+        if (cell.HasRoadThroughEdge(direction))
+        {
+            if (neighbour.Elevation - cell.Elevation == 1)
+                return 0.5f;
+            else
+                return 0.25f;
+        }
+
+        else if (neighbour.Elevation - cell.Elevation == 1)
+            return 2f;
+
+        else if (CrossesRiver(cell, direction))
+            return 2f;
+
+        else return 1f;
+    }
+
+    public bool CrossesRiver(HexCell cell, HexDirection direction)
+    {
+        HexCell neighbour = cell.GetNeighbor(direction);
+
+        // No need to cross river if there isn't one, or if can go around (in case of a river end), or there is a bridge
+        if (neighbour.HasRiver && !neighbour.HasRiverBeginOrEnd && !neighbour.HasRoadThroughEdge(direction))
+        {
+            // For each neighbour of current cell
+            Array directionValues = Enum.GetValues(typeof(HexDirection));
+            for (int i = 0; i < directionValues.Length; i++)
+            {
+                // Direction of adjacent cell
+                HexDirection adjacentDirection = (HexDirection)directionValues.GetValue(i);
+
+                // Except for when moving along river
+                if (adjacentDirection != direction || adjacentDirection != direction.Opposite())
+                {
+                    // If there is a river crossing path, we must cross it
+                    if (neighbour.HasRiverThroughEdge(adjacentDirection))
+                        return true;
+                }
+            }
+        }
+
+        return false;   
     }
 }
