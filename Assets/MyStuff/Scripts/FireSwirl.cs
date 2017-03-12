0cc1dc58fb7b86fc87ff7902f296c163bffd5e92
Stephen <clarksj4@student.op.ac.nz>
2017-03-12 13:10:11 +1300

    Add elemental shield particle effects

---
+++ Assets/MyStuff/Scripts/FireSwirl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireSwirl : MonoBehaviour
+{
+    public float AngularVelocity = 360;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.RotateAround(transform.parent.position, transform.parent.forward, AngularVelocity * Time.deltaTime);
+	}
+}
---
+++ Assets/MyStuff/Scripts/RockBob.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockBob : MonoBehaviour
+{
+    public Transform Rock;
+    public Transform[] Pebbles;
+
+    public float BobTime = 1;
+    public float MaxBob = 0.1f;
+
+    void Start()
+    {
+        StartCoroutine(DoBob(Rock, -1, MaxBob, BobTime));
+        foreach (Transform pebble in Pebbles)
+        {
+            StartCoroutine(DoBob(pebble, OneOrMinusOne(), Random.Range(MaxBob / 2, MaxBob), Random.Range(BobTime / 2, BobTime)));
+        }
+    }
+
+    IEnumerator DoBob(Transform rock, float startDirection, float distance, float time)
+    {
+        Vector3 start = rock.localPosition;
+        float direction = startDirection;
+        float speed = distance / time;
+
+        while (true)
+        {
+            float move = speed * Time.deltaTime * direction;
+            rock.localPosition += Vector3.up * move;
+
+            if (Vector3.Distance(start, rock.localPosition) >= distance)
+            {
+                rock.localPosition = start + (Vector3.up * distance * direction);
+                direction *= -1;
+            }
+
+            yield return null;
+        }
+
+        transform.localPosition = start;
+    }
+
+    float OneOrMinusOne()
+    {
+        return Random.value > 0.5f ? 1 : -1;
+    }
+}
