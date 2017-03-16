7676d2c2ff0bf78c015c9941a6e3fa521c54c5fa
Stephen <clarksj4@student.op.ac.nz>
2017-03-16 16:17:13 +1300

    Rework character input class

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,17 +9,25 @@ public abstract class Player : MonoBehaviour
 
     private List<Character> Actors;
 
+    public bool IsTurn { get { return Actors.Contains(TurnSystem.Current); } }
+
     private void Awake()
     {
         // Get list of all characters this player controls
         Actors = GetComponentsInChildren<Character>().ToList();
+
+        //// Tell each character that this player is its controller
+        //foreach (Character actor in Actors)
+        //    actor.Controller = this;
     }
 
     private void Start()
     {
-        // Tell each character that this player is its controller
         foreach (Character actor in Actors)
+        {
             actor.Controller = this;
+            TurnSystem.Add(actor);
+        }
     }
 
     public void Add(Character actor)
