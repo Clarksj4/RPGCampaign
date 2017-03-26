1e97ce77dabf430fdd69ac9cd46f03659e3df7e7
Stephen <clarksj4@student.op.ac.nz>
2017-03-26 23:43:22 +1300

    Tidy chase behaviour

--- Assets/Scripts/AIBehaviour/ChaseBehaviour.cs
+++ Assets/Scripts/AIBehaviour/ChaseBehaviour.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 public class ChaseBehaviour : AIBehaviour
 {
     private Character target;
-    private HexPath path;
+    private Attack attack;
 
     public ChaseBehaviour(AIPlayer ai) 
         : base(ai)
@@ -19,35 +18,13 @@ public class ChaseBehaviour : AIBehaviour
         base.Activate();
 
         // If not in range of target...
-        Attack attack = Current.Attacks[0];
+        attack = Current.Attacks[0];
         if (!attack.InRange(target.Cell))
-        {
-            // Move to attack range...
-            bool moving = Current.MoveToAttackRange(target, attack);
-
-            // If character is able to move, wait for them to finish moving
-            if (moving)
-                Current.FinishedMovement += Current_FinishedMovement;
-            
-            // Otherwise, end turn.
-            else
-                EndTurn();
-        }
+            MoveOrEndTurn();                // Move to attack range, or end turn if can't move
 
         // Already in range...
         else
-        {
-            // Attack target
-            bool attacking = Current.Attack(target, attack);
-
-            // If able to attack target, wait for attack to finish
-            if (attacking)
-                Current.FinishedAttack += Current_FinishedAttack;
-
-            // Otherwise, end turn
-            else
-                EndTurn();
-        }
+            AttackOrEndTurn();              // Attack, or end turn if unable to
     }
 
     public override void EndTurn()
@@ -58,20 +35,43 @@ public class ChaseBehaviour : AIBehaviour
         base.EndTurn();
     }
 
-    private void Current_FinishedAttack(object sender, EventArgs e)
+    private void MoveOrEndTurn()
     {
-        EndTurn();
+        // Move to attack range...
+        bool moving = Current.MoveToAttackRange(target, attack);
+
+        // If character is able to move, wait for them to finish moving
+        if (moving)
+            Current.FinishedMovement += Current_FinishedMovement;
+
+        // Otherwise, end turn.
+        else
+            EndTurn();
+    }
+
+    private void AttackOrEndTurn()
+    {
+        // Attack target
+        bool attacking = Current.Attack(target, attack);
+
+        // If able to attack target, wait for attack to finish
+        if (attacking)
+            Current.FinishedAttack += Current_FinishedAttack;
+
+        // Otherwise, end turn
+        else
+            EndTurn();
     }
 
     private void Current_FinishedMovement(object sender, CharacterMovementEventArgs e)
     {
-        Attack attack = Current.Attacks[0];
-        if (Current.Stats.CurrentTimeUnits >= attack.Cost)
-        {
-            
-        }
+        Current.FinishedMovement -= Current_FinishedMovement;
+        AttackOrEndTurn();
+    }
 
-        // End turn when character has finished moving
+    private void Current_FinishedAttack(object sender, EventArgs e)
+    {
+        Current.FinishedAttack -= Current_FinishedAttack;
         EndTurn();
     }
 }
