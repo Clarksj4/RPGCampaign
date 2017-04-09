029913503abfa16ca7dc53743dc10da83ec2702f
Stephen <clarksj4@student.op.ac.nz>
2017-04-09 15:00:42 +1200

    Move behaviour tree logic to strategy implmentor

--- Assets/Scripts/AIPlayer.cs
+++ Assets/Scripts/AIPlayer.cs
@@ -7,67 +7,30 @@ using FluentBehaviourTree;
 
 public class AIPlayer : Player
 {
-    private IBehaviourTreeNode behaviourTree;
+    private IBehaviourStrategy behaviour;
 
-    private Character target;
-    private Attack attack;
-    private HexPath path;
-    private bool setAlongPath = false;
-    private bool toldToAttack = false;
-
-    private void Start()
+    protected override void Awake()
     {
-        BehaviourTreeBuilder builder = new BehaviourTreeBuilder();
-        behaviourTree = builder
-            .Sequence("Sequence")
-                
-                // TODO: subtree more intelligently picks a spell for the situation
-                .Do("Pick spell", t => PickSpell())
-                .Sequence("Sequence")
+        base.Awake();
 
-                    // TODO: subtree more intelligently picks a target
-                    .Do("Find target", t => FindTarget())
-                    .Sequence("Sequence")
-                        .Selector("Range")
-                            
-                            // If not in range
-                            .Condition("In range?", t => InRange())
-                            
-                            // Move into range if enough TU 
-                            .Sequence("Get in range")
+        // [PLACEHOLDER] TODO: Behaviour factory assembles behaviour trees and instantiates behaviour classes
+        SetBehaviour(new AggressiveBehaviour());
+    }
 
-                                // Find a path to a cell that is within range of the chosen attack
-                                .Do("Find path to in range position", t => FindPath())
-
-                                // Enough TU to move AND attack
-                                .Condition("Enough TU for move and attack?", t => WithinCost())
-                                
-                                // Do the move
-                                .Do("Move into range", t => FollowPath())
-                            .End()
-                        .End()
-
-                        // Cast the chosen spell / attack if enough TU
-                        .Selector("Cast spell")
-                            .Condition("Enough TU for attack?", t => current.Stats.CurrentTimeUnits >= attack.Cost)
-                            .Do("Attack!", t => Attack())
-                        .End()
-                    .End()
-                .End()
-            .End()
-            .Build();
+    /// <summary>
+    /// Sets this AI's behaviour strategy
+    /// </summary>
+    public void SetBehaviour(IBehaviourStrategy behaviour)
+    {
+        this.behaviour = behaviour;
     }
 
     public override void Activate(Character actor)
     {
         base.Activate(actor);
 
-        // Reset behaviour tree variables
-        target = null;
-        attack = null;
-        path = null;
-        setAlongPath = false;
-        toldToAttack = false;
+        // Behaviour handler is notified that it's its turn
+        behaviour.Activate(actor);
 
         // Traverse tree until tree fails or succeeds
         StartCoroutine(ProcessTurn());
@@ -75,145 +38,21 @@ public class AIPlayer : Player
 
     IEnumerator ProcessTurn()
     {
-        BehaviourTreeStatus status = behaviourTree.Tick(new TimeData(Time.deltaTime));
+        // Behaviour strategy traverses behaviour tree and reports its status
+        BehaviourTreeStatus status = behaviour.Update();
+
+        // Continuously update the behaviour tree until it reports that it is done
         while (status == BehaviourTreeStatus.Running)
         {
             yield return null;
 
-            status = behaviourTree.Tick(new TimeData(Time.deltaTime));
+            status = behaviour.Update();
         }
 
-        //yield return new WaitUntil(() => behaviourTree.Tick(new TimeData(Time.deltaTime)) != BehaviourTreeStatus.Running);
+        // Same as above logic, but condensed to one line; therefore, harder to debug
+        //yield return new WaitUntil(() => behaviour.Update() != BehaviourTreeStatus.Running);
+
+        // End turn once the behaviour tree has completed its actions 
         EndTurn();
-    }
-
-    /// <summary>
-    /// Picks the first spell of the current character's spells.
-    /// </summary>
-    /// <returns>Success if the character has atleast one spell, failure otherwise</returns>
-    private BehaviourTreeStatus PickSpell()
-    {
-        BehaviourTreeStatus result = BehaviourTreeStatus.Failure;
-
-        // If the current character has atleast one attack, use the first one
-        if (current.Attacks.Length > 0)
-        {
-            attack = current.Attacks[0];
-            result = BehaviourTreeStatus.Success;
-        }
-            
-        // Successful if the current character has atleast one attack
-        return result;
-    }
-
-    /// <summary>
-    /// Finds an enemy target to attack. Picks the first non-allied character from the scene hierarchy
-    /// </summary>
-    /// <returns>Success if there is a non-allied character in the scene, failure otherwise</returns>
-    private BehaviourTreeStatus FindTarget()
-    {
-        BehaviourTreeStatus result = BehaviourTreeStatus.Failure;
-
-        // Find a character that is not one of this player's characters
-        List<Character> enemies = GameManager.Characters.Where(c => !characters.Contains(c)).ToList();
-        target = enemies[0];
-
-        // If found an enemey
-        if (target != null)
-            result = BehaviourTreeStatus.Success;
-
-        return result;
-    }
-
-    /// <summary>
-    /// Checks if the chosen attack is in range of the chosen target
-    /// </summary>
-    /// <returns>True if the attack is currently in range</returns>
-    private bool InRange()
-    {
-        bool inRange = attack.InRange(target.Cell);
-        return inRange;
-    }
-
-    /// <summary>
-    /// Finds a path from the current character's position to the quickest cell that is in range of the target for the chosen 
-    /// attack
-    /// </summary>
-    /// <returns>Success if there is a path to an in range cell, failure otherwise</returns>
-    private BehaviourTreeStatus FindPath()
-    {
-        BehaviourTreeStatus result = BehaviourTreeStatus.Failure;
-
-        // Find a path from the current characters cell quickest to reach cell that is in range of the target for the given attack
-        path = Pathfind.ToWithinRange(current.Cell, target.Cell, attack.Range, current.Stats.Traverser, attack.Traverser);
-
-        // Is the path legit?
-        if (path != null && path.Count >= 2)
-            result = BehaviourTreeStatus.Success;
-
-        return result;
-    }
-
-    /// <summary>
-    /// Checks if the current character has enough time units to traverse the path AND then perform the chosen attack
-    /// </summary>
-    /// <returns>True if the current character has enough time units to move and attack</returns>
-    private bool WithinCost()
-    {
-        bool withinCost = current.Stats.CurrentTimeUnits >= path.Cost + attack.Cost;
-        return withinCost;
-    }
-
-    /// <summary>
-    /// Moves the current character along the chosen path.
-    /// </summary>
-    /// <returns>Success when the character has finished moving along the path, RUNNING otherwise</returns>
-    private BehaviourTreeStatus FollowPath()
-    {
-        // Result by default is RUNNING rather than failure
-        BehaviourTreeStatus result = BehaviourTreeStatus.Running;
-
-        // If character hasn't been told to move yet...
-        if (!setAlongPath)
-        {
-            // Move character
-            current.Move(path);
-            setAlongPath = true;
-        }
-
-        // Has character finished moving?
-        else if (!current.IsMoving)
-        {
-            setAlongPath = false;
-            result = BehaviourTreeStatus.Success;
-        }
-
-        return result;
-    }
-
-    /// <summary>
-    /// Orders the current character to attack the chosen target with the chosen spell
-    /// </summary>
-    /// <returns>Success when the character has finished attacking the target, RUNNING otherwise</returns>
-    private BehaviourTreeStatus Attack()
-    {
-        // Result by default is RUNNING rather than failure
-        BehaviourTreeStatus result = BehaviourTreeStatus.Running;
-
-        // Has character finished attack?
-        if (!current.IsAttacking && toldToAttack)
-        {
-            toldToAttack = false;
-            result = BehaviourTreeStatus.Success;
-        }
-
-        // If the character has not been told to attack yet....
-        else if (!toldToAttack)
-        {
-            current.Attack(target, attack);
-            toldToAttack = true;
-        }
-
-        return result;
     }
 }
