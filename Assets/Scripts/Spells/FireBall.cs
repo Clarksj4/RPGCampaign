7f864e7ab8a7a6cd6c026fe42888dfc174199ad9
Stephen <clarksj4@student.op.ac.nz>
2017-04-13 00:35:01 +1200

    Move fireball script to spell folder

---
+++ Assets/Scripts/Spells/FireBall.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBall : Spell
+{
+    public Vector3 DetonationPosition;
+    public float speed = 10;
+
+    public override void Cast(Character caster, HexCell target)
+    {
+        GameObject instance = Instantiate(gameObject, caster.transform.position + caster.CastPosition, Quaternion.identity) as GameObject;
+
+        Spell spell = instance.GetComponent<Spell>();
+        spell.caster = caster;
+        spell.target = target;
+
+        spell.transform.LookAt(target.Position);
+    }
+
+    void Update ()
+    {
+        Vector3 movementSpeed = Vector3.MoveTowards(transform.position, target.Position + DetonationPosition, speed * Time.deltaTime);
+        transform.position = movementSpeed;
+
+        // TODO: Destroy upon arriving at destination
+    }
+}
