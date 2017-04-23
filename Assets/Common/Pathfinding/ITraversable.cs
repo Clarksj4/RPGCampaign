c058531493ff1ed5c3f1bf1675abcd7c09aa4b5d
Clarksj4 <clarksj4@student.op.ac.nz>
2017-04-23 23:29:04 +1200

    Rename interface method arguements

--- Assets/Common/Pathfinding/ITraversable.cs
+++ Assets/Common/Pathfinding/ITraversable.cs
@@ -10,17 +10,17 @@ namespace Pathfinding
         /// Can a path travel between the two adjacent nodes?
         /// </summary>
         /// <param name="begin">The beginning node</param>
-        /// <param name="adjacent">The destination node</param>
+        /// <param name="next">The destination node</param>
         /// <returns>True if traversal is allowed from the beginning node to the adjacent node</returns>
-        bool IsTraversable(IGraphNode begin, IGraphNode adjacent);
+        bool IsTraversable(IGraphNode begin, IGraphNode next);
 
         /// <summary>
         /// What costs is associated with travelling between the two adjacent nodes?
         /// </summary>
         /// <param name="begin">The beginning node</param>
-        /// <param name="adjacent">The destination node</param>
+        /// <param name="next">The destination node</param>
         /// <returns>The cost associated with travelling from the beginning node to the adjacent 
         /// node</returns>
-        float Cost(IGraphNode begin, IGraphNode adjacent);
+        float Cost(IGraphNode begin, IGraphNode next);
     }
 }
