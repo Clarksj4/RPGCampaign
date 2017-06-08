482b4a8547059db17c86a9bd279617a93516507f
Stephen <clarksj4@student.op.ac.nz>
2017-06-08 15:11:03 +1200

    Add background music

---
+++ Assets/BackgroundMusic.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroundMusic : MonoBehaviour
+{
+
+    public AudioClip[] Clips;
+
+    private AudioSource player;
+
+	// Use this for initialization
+	void Start ()
+    {
+        player = GetComponent<AudioSource>();
+
+        if (Clips != null && Clips.Length > 0)
+        {
+            // Get random index
+            int index = Random.Range(0, Clips.Length);
+
+            // Use as clip
+            player.clip = Clips[index];
+
+            player.Play();
+        }
+	}
+
+    public void SliderChanged(Slider slider)
+    {
+        player.volume = slider.value;
+    }
+
+    public void Mute(Toggle toggle)
+    {
+        player.mute = !toggle.isOn;
+    }
+}
